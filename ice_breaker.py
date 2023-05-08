5cb2ea9c5f3180144f766d388f542dca7d987c09
Ken Ling <ken88ling@gmail.com>
2023-05-08 14:50:23 +0800

    AGENTS-LOOKUP

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,8 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
 
@@ -21,9 +23,11 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
+    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
+
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
+        # linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
+        linkedin_profile_url=linkedin_profile_url
     )
 
     print(chain.run(information=linkedin_data))
-
